a891c7e407df4e46743b2aaf39372394c72f1521
cezannec <cezanne.camacho@gmail.com>
2016-10-13 18:14:30 -0700

    T09.02-Exercise-AddURIsToContract

---
+++ T09.02-Exercise-AddURIsToContract/app/src/main/java/com/example/android/todolist/data/TaskContract.java
@@ -0,0 +1,55 @@
+package com.example.android.todolist.data;
+
+import android.provider.BaseColumns;
+
+
+public class TaskContract {
+
+
+    /* TaskEntry is an inner class that defines the contents of the task table */
+    public static final class TaskEntry implements BaseColumns {
+
+        /* TODO: Add content provider constants to the Contract
+         Clients need to know how to access the task data, and it's your job to provide
+         these content URI's for the path to that data:
+            1) Content authority,
+            2) Base content URI,
+            3) Path(s) to the tasks directory
+            4) Content URI for data in the TaskEntry class
+        */
+
+
+        // Task table and column names
+        public static final String TABLE_NAME = "tasks";
+
+        // Since TaskEntry implements the interface "BaseColumns", it has an automatically produced
+        // "_ID" column in addition to the two below
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_PRIORITY = "priority";
+
+
+        /*
+        The above table structure looks something like the sample table below.
+        With the name of the table and columns on top, and potential contents in rows
+
+        Note: Because this implements BaseColumns, the _id column is generated automatically
+
+        tasks
+         - - - - - - - - - - - - - - - - - - - - - -
+        | _id  |    description     |    priority   |
+         - - - - - - - - - - - - - - - - - - - - - -
+        |  1   |  Complete lesson   |       1       |
+         - - - - - - - - - - - - - - - - - - - - - -
+        |  2   |    Go shopping     |       3       |
+         - - - - - - - - - - - - - - - - - - - - - -
+        .
+        .
+        .
+         - - - - - - - - - - - - - - - - - - - - - -
+        | 43   |   Learn guitar     |       2       |
+         - - - - - - - - - - - - - - - - - - - - - -
+
+         */
+
+    }
+}

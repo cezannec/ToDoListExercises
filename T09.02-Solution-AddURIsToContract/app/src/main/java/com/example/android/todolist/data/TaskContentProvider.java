a7d032f590e9c0f9698d30f12082ff5939400fd9
cezannec <cezanne.camacho@gmail.com>
2016-10-13 18:21:27 -0700

    T09.02-Solution-AddURIsToContract

---
+++ T09.02-Solution-AddURIsToContract/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -0,0 +1,66 @@
+package com.example.android.todolist.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+
+// (COMPLETE): Verify that TaskContentProvider extends from ContentProvider and implements required methods
+public class TaskContentProvider extends ContentProvider {
+
+
+    /* onCreate() is where you should initialize anything you’ll need to setup
+    your underlying data source.
+    In this case, you’re working with a SQLite database, so you’ll need to
+    initialize a DbHelper to gain access to it.
+     */
+    @Override
+    public boolean onCreate() {
+        // (COMPLETE): Complete onCreate() and initialize a TaskDbhelper on startup
+        // [Hint] Declare the DbHelper as a global variable
+
+        Context context = getContext();
+        mTaskDbHelper = new TaskDbHelper(context);
+        return true;
+    }
+
+
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}

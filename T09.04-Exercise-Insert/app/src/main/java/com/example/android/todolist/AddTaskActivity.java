fdc458d4749f166213c5dc788c6c508bad06bb7f
cezannec <cezanne.camacho@gmail.com>
2016-10-13 20:03:59 -0700

    T09.04-Exercise-Insert

---
+++ T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -0,0 +1,53 @@
+package com.example.android.todolist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    // Declare a member variable to keep track of a task's selected mPriority
+    private int mPriority;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        // Initialize to highest mPriority by default (mPriority = 1)
+        ((RadioButton) findViewById(R.id.radButton1)).setChecked(true);
+        mPriority = 1;
+    }
+
+
+    /**
+     * onClickAddTask is called when the "ADD" button is clicked.
+     * It retrieves user input and inserts that new task data into the underlying database.
+     */
+    public void onClickAddTask(View view) {
+        // Not yet implemented
+        // TODO: 6. Check if EditText is empty, if not retrieve input and store it in a ContentValues object
+
+        // TODO: 7. Insert new task data via a ContentResolver
+
+        // TODO: 8. Display the URI that's returned with a Toast
+        // [Hint] Don't forget to call finish() to return to MainActivity after this insert is complete
+    }
+
+
+    /**
+     * onPrioritySelected is called whenever a priority button is clicked.
+     * It changes the value of mPriority based on the selected button.
+     */
+    public void onPrioritySelected(View view) {
+        if (((RadioButton) findViewById(R.id.radButton1)).isChecked()) {
+            mPriority = 1;
+        } else if (((RadioButton) findViewById(R.id.radButton2)).isChecked()) {
+            mPriority = 2;
+        } else if (((RadioButton) findViewById(R.id.radButton3)).isChecked()) {
+            mPriority = 3;
+        }
+    }
+}
---
+++ T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -0,0 +1,111 @@
+package com.example.android.todolist.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+
+// (COMPLETE): Verify that TaskContentProvider extends from ContentProvider and implements required methods
+public class TaskContentProvider extends ContentProvider {
+
+    // (COMPLETE): Define final integer constants for the directory of tasks and a single item.
+    // It's convention to use 100, 200, 300, etc for directories,
+    // and related ints (101, 102, ..) for items in that directory.
+    public static final int TASKS = 100;
+    public static final int TASK_WITH_ID = 101;
+
+    // (COMPLETE): Declare a static variable for the Uri matcher that you construct
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    // (COMPLETE): Define a static buildUriMatcher method that associates URI's with their int match
+    /**
+     Initialize a new matcher object without any matches,
+     then use .addURI(String authority, String path, int match) to add matches
+     */
+    public static UriMatcher buildUriMatcher() {
+
+        // Initialize a UriMatcher with no matches by passing in NO_MATCH to the constructor
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        /*
+          All paths added to the UriMatcher have a corresponding int.
+          For each kind of uri you may want to access, add the corresponding match with addURI.
+          The two calls below add matches for the task directory and a single item by ID.
+         */
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASKS);
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS + "/#", TASK_WITH_ID);
+
+        return uriMatcher;
+    }
+
+
+    // Member variable for a TaskDbHelper that's initialized in the onCreate() method
+    private TaskDbHelper mTaskDbHelper;
+
+    /* onCreate() is where you should initialize anything you’ll need to setup
+    your underlying data source.
+    In this case, you’re working with a SQLite database, so you’ll need to
+    initialize a DbHelper to gain access to it.
+     */
+    @Override
+    public boolean onCreate() {
+        // (COMPLETE): Complete onCreate() and initialize a TaskDbhelper on startup
+        // [Hint] Declare the DbHelper as a global variable
+
+        Context context = getContext();
+        mTaskDbHelper = new TaskDbHelper(context);
+        return true;
+    }
+
+
+    // TODO: Implement insert to handle requests to insert a single new row of data
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+
+        // TODO: 1. Get access to the task database (to write new data to)
+
+        // TODO: 2. Write URI matching code to identify the match for the tasks directory
+
+        // TODO: 3. Insert new values into the database
+        // TODO: 4. Set the value for the returnedUri and write the default case for unknown URI's
+
+        // TODO: 5. Notify the resolver if the uri has been changed
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
